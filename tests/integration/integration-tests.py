4993daec3a64fef78c9627294495d45dd591feb1
Bernd Aufrecht <b.aufrecht@openthinclient.com>
2017-01-18 12:41:50 +0100

    Added additional integration tests for GUI customization
    - Added initial lightdm tests
    - Added tests for desktop background and icons
    - Updated test_packages routine
    - initial package cleanup tests

--- tests/integration/integration-tests.py
+++ tests/integration/integration-tests.py
@@ -4,7 +4,7 @@ import pytest
     ("mysql-server", "5.5"),
     ("python", "2.7"),
 ])
-def test_packages(Package, name, version):
+def test_basic_packages_installed(Package, name, version):
     assert Package(name).is_installed
     assert Package(name).version.startswith(version)
 
@@ -61,8 +61,174 @@ def test_openthinclient_manager_file(File):
     ("/usr/local/bin/openthinclient-vmversion"),
 ])
 
-def test_otc_local_bin_files(File, filename):
+def test_otc_usr_local_bin_files(File, filename):
     file = File(filename)
     assert file.user == "openthinclient"
     assert file.group == "openthinclient"
     assert file.exists == True
+
+
+@pytest.mark.parametrize("filename", [
+    ("/usr/local/sbin/openthinclient-changepassword"),
+    ("/usr/local/sbin/openthinclient-cleaner"),
+    ("/usr/local/sbin/openthinclient-edit-sources-lst-lite"),
+    ("/usr/local/sbin/openthinclient-ldapbackup"),
+    ("/usr/local/sbin/openthinclient-restart"),
+    ("/usr/local/sbin/zerofree.sh"),
+])
+
+def test_otc_usr_local_sbin_files(File, filename):
+    file = File(filename)
+    assert file.user == "openthinclient"
+    assert file.group == "openthinclient"
+    assert file.exists == True
+
+def test_crond_ldap_backup_file(File):
+    managerbin = File("/etc/cron.d/openthinclient_ldap_backup")
+    assert managerbin.user == "root"
+    assert managerbin.group == "root"
+    assert managerbin.exists == True
+
+
+@pytest.mark.parametrize("filename,content", [
+    ("/etc/sudoers.d/90-openthinclient-appliance", "openthinclient ALL=(ALL) NOPASSWD:ALL"),
+])
+
+def test_sudoers_file(File, filename, content, Command, Sudo):
+    file = File(filename)
+    with Sudo():
+        Command.check_output("whoami")
+        assert file.contains(content)
+        assert file.user == "root"
+        assert file.group == "root"
+        assert file.exists == True
+
+
+@pytest.mark.parametrize("filename", [
+    ("/etc/X11/Xsession.d/21-lightdm-locale-fix"),
+])
+
+def test_otc_gui_lightdm_locale_fix(File, filename):
+    file = File(filename)
+    assert file.user == "root"
+    assert file.group == "root"
+    assert file.exists == True
+    # assert file.mode == 0o744 # FIXME - check if this needs to executable
+
+
+@pytest.mark.parametrize("filename", [
+    ("/etc/lightdm/lightdm.conf"),
+])
+
+def test_lightdm_config_file(File, filename):
+    file = File(filename)
+    #assert file.user == "root"
+    #assert file.group == "root"
+    assert file.exists == True
+
+
+
+@pytest.mark.parametrize("filename,content", [
+    ("/etc/lightdm/lightdm.conf", "greeter-setup-script=/usr/local/bin/openthinclient-default-user-fix" ),
+    ("/etc/lightdm/lightdm.conf", "allow-guest=false"),
+    ("/etc/lightdm/lightdm.conf", "greeter-hide-users=false"),
+    ("/etc/lightdm/lightdm.conf", "greeter-show-manual-login=true"),
+])
+
+def test_lightdm_config_content(File, filename, content):
+    file = File(filename)
+    assert file.contains(content)
+    #assert file.group == "root"
+    assert file.exists == True
+
+
+@pytest.mark.parametrize("filename,content", [
+    ("/etc/lightdm/lightdm-gtk-greeter.conf",
+        "background=/usr/local/share/openthinclient/backgrounds/openthinclient-server-Desktop-Pales.jpg" ),
+    ("/etc/lightdm/lightdm-gtk-greeter.conf", "show-clock=true"),
+])
+
+def test_lightdm_config_content(File, filename, content):
+    file = File(filename)
+    assert file.contains(content)
+    #assert file.group == "root"
+    assert file.exists == True
+
+
+@pytest.mark.parametrize("filename", [
+    ("/usr/local/bin/openthinclient-default-user-fix"),
+    ("/usr/local/bin/openthinclient-keyboard-layout-fix"),
+    ("/home/openthinclient/.config/autostart/keyboard-layout-fix.desktop"),
+])
+
+def test_otc_gui_fixes_via_script(File, filename):
+    file = File(filename)
+    assert file.user == "openthinclient"
+    assert file.group == "openthinclient"
+    assert file.exists == True
+
+
+@pytest.mark.parametrize("filename", [
+    ("/home/openthinclient/Desktop/Buy hardware.desktop"),
+    ("/home/openthinclient/Desktop/change password.desktop"),
+    ("/home/openthinclient/Desktop/Edit openthinclient package sources.desktop"),
+    ("/home/openthinclient/Desktop/Feature Bid.desktop"),
+    ("/home/openthinclient/Desktop/livesupport.levigo.de.desktop"),
+    ("/home/openthinclient/Desktop/mate-network-properties.desktop"),
+    ("/home/openthinclient/Desktop/mate-time.desktop"),
+    ("/home/openthinclient/Desktop/openthinclient Manager.desktop"),
+    ("/home/openthinclient/Desktop/openthinclient Package Manager.desktop"),
+    ("/home/openthinclient/Desktop/openthinclient service restart.desktop"),
+    ("/home/openthinclient/Desktop/Oracle-Java-Licence"),
+    ("/home/openthinclient/Desktop/professional support & hardware.desktop"),
+    ("/home/openthinclient/Desktop/README.desktop"),
+    #("/home/openthinclient/Desktop/teamviewer-teamviewer9.desktop"),
+    ("/home/openthinclient/Desktop/Version-Information.desktop"),
+    ("/home/openthinclient/Desktop/VNC Viewer.desktop"),
+])
+
+def test_otc_desktop_icons(File, filename):
+    file = File(filename)
+    assert file.user == "openthinclient"
+    assert file.group == "openthinclient"
+    assert file.exists == True
+
+
+@pytest.mark.parametrize("filename", [
+    ("/usr/local/share/openthinclient/backgrounds/openthinclient-server-Desktop-Pales.jpg"),
+    ("/usr/local/share/openthinclient/backgrounds/desktopB_1920x1200.png"),
+    ("/usr/local/share/openthinclient/backgrounds/OTC_VM_1280x1024.png"),
+    ("/usr/local/share/openthinclient/icons/openthinclient_advisor.png"),
+    ("/usr/local/share/openthinclient/icons/openthinclient_ceres_version.png"),
+    ("/usr/local/share/openthinclient/icons/openthinclient_consus_version.png"),
+    ("/usr/local/share/openthinclient/icons/openthinclient-features.png"),
+    ("/usr/local/share/openthinclient/icons/openthinclient_manager.png"),
+    ("/usr/local/share/openthinclient/icons/openthinclient_minerva_version.png"),
+    ("/usr/local/share/openthinclient/icons/openthinclient_pales_version.png"),
+    ("/usr/local/share/openthinclient/icons/openthinclient_professional_support.png"),
+    ("/usr/local/share/openthinclient/icons/openthinclient_readme.png"),
+    ("/usr/local/share/openthinclient/icons/openthinclient_service_restart.png"),
+    ("/usr/local/share/openthinclient/icons/openthinclient_shop.png"),
+])
+
+def test_otc_background_and_icons(File, filename):
+    file = File(filename)
+    assert file.user == "openthinclient"
+    assert file.group == "openthinclient"
+    assert file.exists == True
+
+@pytest.mark.parametrize("name,version", [
+    ("mate-desktop-environment-core", "1.8"),
+    ("lightdm", "1.10"),
+])
+
+def test_gui_packages_installed(Package, name, version):
+    assert Package(name).is_installed
+    assert Package(name).version.startswith(version)
+
+
+@pytest.mark.parametrize("name", [
+    ("rpcbind"),
+])
+def test_package_cleanup(Package, name):
+    assert Package(name).is_installed == False

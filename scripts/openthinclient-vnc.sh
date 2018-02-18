06331fbda3df51c468aeaade1c6305c7b46d37a7
Bernd Aufrecht <b.aufrecht@openthinclient.com>
2018-02-18 14:43:49 +0100

    Removed unused fluxbox window manager and all corresponding tests

--- scripts/openthinclient-vnc.sh
+++ scripts/openthinclient-vnc.sh
@@ -5,7 +5,7 @@
 
 INSTALL="apt-get install -y"
 UPDATE="apt-get update"
-PACKAGES="fluxbox openbox"
+PACKAGES="openbox"
 
 eval "$UPDATE"
 eval "$INSTALL $PACKAGES"
@@ -20,38 +20,6 @@ apt-get install -y --no-install-recommends xvfb
 
 echo "==> Installing x11vnc with --no-install-recommends"
 apt-get install -y --no-install-recommends x11vnc
-
-
-# fluxbox configuration
-read -r -d '' WALLINCLUDE << EOF
-$full $full|/usr/local/share/openthinclient/backgrounds/openthinclient-server-Desktop-Pales.jpg||:22.0
-EOF
-
-echo "==> Creating .flubxbox in openthinclient home directory"
-mkdir /home/openthinclient/.fluxbox/
-
-echo "==> Including custom wallpaper information for fluxbox "
-echo "${WALLINCLUDE}" > ${FBOX_LASTWALLPAPER}
-
-echo "==> Including custom wallpaper information for fluxbox via overlay"
-cat > /home/openthinclient/.fluxbox/overlay << EOF
-background: aspect
-background.pixmap: /usr/local/share/openthinclient/backgrounds/openthinclient-server-Desktop-Pales.jpg
-EOF
-
-echo "==> Including custom fluxbox startup file"
-cat > /home/openthinclient/.fluxbox/startup << EOF
-#fbsetbg -l # sets the last background set, very useful and recommended.
-# In the below commands the ampersand symbol (&) is required on all applications that do not terminate immediately.
-# Failure to provide them will cause Fluxbox not to start.
-/usr/local/bin/openthinclient-manager &
-# exec is for starting Fluxbox itself, do not put an ampersand (&) after this or Fluxbox will exit immediately.
-exec /usr/bin/fluxbox
-# or if you want to keep a log, uncomment the below command and comment out the above command:
-#exec /usr/bin/fluxbox -log ~/.fluxbox/log
-EOF
-
-chown openthinclient:openthinclient /home/openthinclient/.fluxbox/ -R
 
 # openbox configuration
 echo "==> Creating openbox config dir"
--- tests/integration/test_vnc-server.py
+++ tests/integration/test_vnc-server.py
@@ -4,7 +4,6 @@ import pytest
 @pytest.mark.parametrize("name,version", [
     ("x11vnc ", "0.9"),
     ("xvfb ", "2:1"),
-    ("fluxbox ", "1.3"),
     ("openbox", "3.6")
 ])
 def test_vnc_packages_installed(host, name, version):
@@ -14,8 +13,8 @@ def test_vnc_packages_installed(host, name, version):
 
 
 @pytest.mark.parametrize("service_name", [
-    ("x11vnc"),
-    ("xvfb"),
+    "x11vnc",
+    "xvfb",
 ])
 def test_service_running(host, service_name):
     service = host.service(service_name)
@@ -27,8 +26,8 @@ def test_service_running(host, service_name):
     ("tcp", "0.0.0.0", "5900"),
 ])
 def test_vnc_server_listening(host, proto, hostname, port):
-    socketoptions = "{0}://{1}:{2}".format(proto, hostname, port)
-    socket = host.socket(socketoptions)
+    socket_options = "{0}://{1}:{2}".format(proto, hostname, port)
+    socket = host.socket(socket_options)
     assert socket.is_listening
 
 
@@ -47,8 +46,8 @@ def test_xvfb_service_file_present(host):
 
 
 @pytest.mark.parametrize("filename", [
-    ("/usr/local/bin/openthinclient-vnc-starter"),
-    ("/usr/local/bin/openthinclient-vnc-xvfb"),
+    "/usr/local/bin/openthinclient-vnc-starter",
+    "/usr/local/bin/openthinclient-vnc-xvfb",
 ])
 def test_otc_usr_local_bin_vnc_starter(host, filename):
     filen = host.file(filename)
@@ -65,37 +64,6 @@ def test_openbox_settings_files_present(host, filename):
     assert filen.user == "openthinclient"
     assert filen.group == "openthinclient"
     assert filen.exists is True
-
-
-@pytest.mark.parametrize("filename", [
-    "/home/openthinclient/.fluxbox/overlay",
-    "/home/openthinclient/.fluxbox/lastwallpaper",
-    "/home/openthinclient/.fluxbox/startup",
-])
-def test_fluxbox_settings_files_present(host, filename):
-    filen = host.file(filename)
-    assert filen.user == "openthinclient"
-    assert filen.group == "openthinclient"
-    assert filen.exists is True
-
-
-@pytest.mark.parametrize("filename,expected_output", [
-    ("/home/openthinclient/.fluxbox/overlay",
-     "background: aspect \n"
-     "background.pixmap: /usr/local/share/openthinclient/backgrounds/openthinclient-server-Desktop-Pales.jpg"),
-])
-def test_fluxbox_overlay_file_settings(filename, expected_output, host):
-        filen = host.file(filename)
-        assert filen.contains(expected_output)
-
-
-@pytest.mark.parametrize("filename,expected_output", [
-    ("/home/openthinclient/.fluxbox/startup",
-     "/usr/local/bin/openthinclient-manager &"),
-])
-def test_fluxbox_startup_file_contents(filename, expected_output, host):
-        filen = host.file(filename)
-        assert filen.contains(expected_output)
 
 
 @pytest.mark.parametrize("filename,content", [

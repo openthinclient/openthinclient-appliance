6bc6e5481bfac2440fe4ee0b21f861c78c6f81bc
Bernd Aufrecht <b.aufrecht@openthinclient.com>
2018-06-12 21:15:07 +0200

    SOFTWARE-696 - Use predefined profiles from distributions-appliance.xml
    - installer log is now redirected to stdout

--- scripts/openthinclient-installer.sh
+++ scripts/openthinclient-installer.sh
@@ -48,11 +48,12 @@ if [ -f $OTC_INSTALLER_FULLPATH ]; then
 	        $OTC_INSTALL_PATH/bin/managerctl prepare-home \
 	        --admin-password $OTC_DEFAULT_PASS \
 	        --home $OTC_INSTALL_HOME \
+	        --dist-source http://archive.openthinclient.org/openthinclient/distributions-appliance.xml \
 	        --db MYSQL \
 	        --db-host localhost \
 	        --db-name openthinclient \
 	        --db-user openthinclient \
-	        --db-password openthinclient > /dev/null 2>&1
+	        --db-password openthinclient #> /dev/null 2>&1
         else
             echo "==> Running managerctl install with predefined variables and default included H2 database"
             $OTC_INSTALL_PATH/bin/managerctl prepare-home --admin-password $OTC_DEFAULT_PASS --home $OTC_INSTALL_HOME  > /dev/null 2>&1

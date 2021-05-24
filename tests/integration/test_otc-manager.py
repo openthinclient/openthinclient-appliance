eff7f2613d3fd29a62873b468d2ee267fe15f5d6
Bernd Aufrecht <b.aufrecht@openthinclient.com>
2021-05-24 21:01:26 +0200

    OTC-950: remove obsolet test for <server-id> on startup

--- tests/integration/test_otc-manager.py
+++ tests/integration/test_otc-manager.py
@@ -106,16 +106,6 @@ def test_if_otc_manager_default_install_packages_exists(host, filename):
     assert file.exists is True
 
 
-@pytest.mark.parametrize("filename,content", [
-    (otc_manager_install_home + ".otc-manager-home.meta", "<server-id>"),
-])
-def test_otc_manager_metadata_file_for_server_id_present(host, filename, content):
-    time.sleep(20)
-    filen = host.file(filename)
-    assert filen.exists is True
-    assert filen.contains(content) is True
-
-
 @pytest.mark.parametrize("proto,port", [
     ("tcp", "10389"),
     ("tcp", "8080"),

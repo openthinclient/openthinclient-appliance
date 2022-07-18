c8c64076968e59279c54cafb8da29ab4aec45519
Alexander Nickstädt <a.nickstaedt@openthinclient.com>
2022-07-18 16:26:42 +0200

    Add LDAP backup

--- scripts/openthinclient-custom.sh
+++ scripts/openthinclient-custom.sh
@@ -8,6 +8,10 @@ OTC_CUSTOM_DEPLOY_PATH=/tmp/data/otc-custom-deploy
 
 OTC_INSTALL_PATH=/opt/otc-manager/
 
+echo "==> Deploying LDAP backup"
+mkdir -p /etc/skel_ldap/
+cp -a ${OTC_CUSTOM_DEPLOY_PATH}/etc/skel_ldap/ldap_empty.zip /etc/skel_ldap/
+
 echo "==> Deploying htop preconfiguration file"
 cp -a ${OTC_CUSTOM_DEPLOY_PATH}/etc/htoprc /etc/
 

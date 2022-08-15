732fe7ef4600584bc9ae22b0fd8b8292dbdeb7c5
Alexander Nickstädt <a.nickstaedt@openthinclient.com>
2022-08-15 17:55:22 +0200

    Fix caja-desktop resolution resize

--- scripts/openthinclient-custom.sh
+++ scripts/openthinclient-custom.sh
@@ -101,12 +101,17 @@ if ! [ -d $OTCLOCALSHARE ]; then
 fi
 
 echo "==> Copying custom bin scripts to /usr/local/bin"
-cp -a ${OTC_CUSTOM_DEPLOY_PATH}/usr/local/bin/openthinclient-manager /usr/local/bin/openthinclient-manager
+cp -a ${OTC_CUSTOM_DEPLOY_PATH}/usr/local/bin/caja-Desktop-resize-Fix /usr/local/bin/caja-Desktop-resize-Fix
 cp -a ${OTC_CUSTOM_DEPLOY_PATH}/usr/local/bin/openthinclient-vmversion /usr/local/bin/openthinclient-vmversion
 
 echo "==> Setting executable bit for custom bin scripts in /usr/local/bin"
+chmod +x /usr/local/bin/caja-Desktop-resize-Fix
 chmod +x /usr/local/bin/openthinclient*
 dos2unix /usr/local/bin/openthinclient*
+
+echo "==> Copying caja-Desktop-resize-Fix.desktop to /etc/xdg/autostart"
+cp -a ${OTC_CUSTOM_DEPLOY_PATH}/etc/xdg/autostart/caja-Desktop-resize-Fix.desktop /etc/xdg/autostart/caja-Desktop-resize-Fix.desktop
+chown root:root /etc/xdg/autostart/caja-Desktop-resize-Fix.desktop
 
 echo "==> Copying custom tcos-ascii script to /usr/local/bin"
 cp -a ${OTC_CUSTOM_DEPLOY_PATH}/usr/local/bin/tcos-ascii /usr/local/bin/tcos-ascii

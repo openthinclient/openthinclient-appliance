d2cdbe95c6693a75d364e4e4914a5ae79143bbbd
Alexander Nickstädt <a.nickstaedt@openthinclient.com>
2025-09-03 12:22:45 +0200

    Update bash syntax in minimize.sh
     - x-prefix in comparisons no longer serves a purpose
    
    Signed-off-by: Alexander Nickstädt <a.nickstaedt@openthinclient.com>

--- scripts/minimize.sh
+++ scripts/minimize.sh
@@ -5,7 +5,7 @@
 
 swapuuid="$(/sbin/blkid -o value -l -s UUID -t TYPE=swap)";
 
-if [ "x${swapuuid}" != "x" ]; then
+if [ -n "$swapuuid" ]; then
     # Whiteout the swap partition to reduce box size
     # Swap is disabled till reboot
     swappart="$(readlink -f /dev/disk/by-uuid/"$swapuuid")";

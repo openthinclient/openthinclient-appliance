9e4b348c8b34a1e65558c3445866cba6d11203bd
Alexander Nickstädt <a.nickstaedt@openthinclient.com>
2025-12-18 12:15:18 +0100

    Use legacy eth0 naming and hand network control to NetworkManager
    Signed-off-by: Alexander Nickstädt <a.nickstaedt@openthinclient.com>

--- scripts/openthinclient-network-config.sh
+++ scripts/openthinclient-network-config.sh
@@ -7,8 +7,6 @@ ln -sf /dev/null /etc/udev/rules.d/80-net-setup-link.rules
 
 # Write minimal network interfaces config
 cat <<'EOF' > /etc/network/interfaces
-# This file describes the network interfaces available on your system
-# and how to activate them. For more information, see interfaces(5).
 
 source /etc/network/interfaces.d/*
 
@@ -16,8 +14,4 @@ source /etc/network/interfaces.d/*
 auto lo
 iface lo inet loopback
 
-# The primary network interface
-auto eth0
-iface eth0 inet dhcp
-pre-up sleep 2
 EOF
